3ffa25f978b3e55f931d62ce6362f08f5f4c1faf
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-02-06 23:40:12 +0100

    chore-trying command

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 const { app } = require('./config/__init_app');
 const MessageListener = require('../app/src/listeners/command.listener');
 
-MessageListener.listen;
+app.command('/bot', async({body, ack, say}) => {
+  await ack();
+  await say("Hello");
+});
 (async () => {
   // Start the app
   await app.start(process.env.PORT || 80);
