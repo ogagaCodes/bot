d92c51bb9f54b4ff9e4d5eb0c2a7713c851768d7
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-02-06 23:46:36 +0100

    chore-trying command

--- app/server.js
+++ app/server.js
@@ -1,15 +1,20 @@
-const { app } = require('./config/__init_app');
-const MessageListener = require('../app/src/listeners/command.listener');
+const { app } = require("./config/__init_app");
+const MessageListener = require("../app/src/listeners/command.listener");
 
-app.command('/bot', async({body, ack, say}) => {
+app.use("/", async ({ body, ack, say }) => {
   await ack();
   await say("Hello");
 });
+
+// app.command('/bot', async({body, ack, say}) => {
+//   await ack();
+//   await say("Hello");
+// });
 (async () => {
   // Start the app
   await app.start(process.env.PORT || 80);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
 
 module.exports = app;
