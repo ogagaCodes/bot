27d27a1eb3cb98d60fa66b5e4384768521c11ff9
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-02-06 19:15:33 +0100

    ft-Set up bot independently

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const { app } = require('./config/__init_app');
 
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 80);
 
   console.log('⚡️ Bolt app is running!');
 })();
