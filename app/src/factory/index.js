bf5ee64a7dd2dcd113230ed574bca2e9873d076a
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-02-06 19:45:29 +0100

    ft-Set up bot independently

--- app/src/factory/index.js
+++ app/src/factory/index.js
@@ -1,12 +1,12 @@
- exports.ListenerFactory = class {
-    constructor(app, event, name, callBack){
-        this.app = app;
-        this.event = event;
-        this.name = name;
-        this.calBack =- callBack
-    }
+exports.ListenerFactory = class {
+  constructor(app, event, name, callBack) {
+    this.app = app;
+    this.event = event;
+    this.name = name;
+    this.calBack = -callBack;
+  }
 
-    listen(){
-        this.app.event(this.name, this.callBack)
-    }
-}
+  listen() {
+    return this.app.event(this.name, this.callBack);
+  }
+};
--- app/src/listeners/command.listener.js
+++ app/src/listeners/command.listener.js
@@ -1,11 +1,18 @@
-const { app } = require('../../config/__init_app');
-const { ListenerFactory } = require('../factory');
-const { dropDowndata } = require('../../helpers/slashCommandData');
+const { app } = require("../../config/__init_app");
+const { ListenerFactory } = require("../factory");
+const { dropDowndata } = require("../../helpers/slashCommandData");
 
-
-const callBackFn =  async ({ body, ack, say }) => {
+// const callBackFn =  async ({ body, ack, say }) => {
+//     await ack();
+//     await say(dropDowndata);
+//   };
+const MessageListener = new ListenerFactory(
+  app,
+  command,
+  "/bot",
+  async ({ body, ack, say }) => {
     await ack();
     await say(dropDowndata);
-  };
-const MessageListener = new ListenerFactory(app, command, '/bot hello',  callBackFn);
+  }
+);
 MessageListener.listen();
